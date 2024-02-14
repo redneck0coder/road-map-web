03ff175e2d1793eeb97aa321a59dabba7ca9cf78
vm <vladm1225@yandex.ru>
2024-02-14 11:27:56 +0400

    update main

--- src/app/pages/main/main.component.ts
+++ src/app/pages/main/main.component.ts
@@ -1,15 +1,21 @@
-import { Component } from '@angular/core';
-import {RouterOutlet} from "@angular/router";
+import {Component, inject} from '@angular/core';
+import {RouterLink, RouterOutlet} from "@angular/router";
+import {CardModule} from "primeng/card";
+import {TopicsService} from "../../shared/services/topics.service";
 
 @Component({
   selector: 'rm-main',
   standalone: true,
   imports: [
-    RouterOutlet
+    RouterOutlet,
+    CardModule,
+    RouterLink
   ],
   templateUrl: './main.component.html',
   styleUrl: './main.component.scss'
 })
 export class MainComponent {
+
+  topicService = inject(TopicsService);
 
 }
